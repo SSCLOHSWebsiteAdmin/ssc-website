734b54f8ea68bdad20b0b721bb8427ddd9a5ce55
Sam Low <samuel.m.d.low@gmail.com>
2021-11-29 23:46:59 -0700

    Added pages for everything in navigation

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ def index():
 def home():
     return render_template('home.html', name="Home")
 
+@app.route('/ShowYourSolidarity')
+def SYS():
+    return render_template('SYS.html', name="Show Your Solidarity")
+
+@app.route('/issues')
+def issues():
+    return render_template('issues.html', name="Issues")
+
+@app.route('/CurrentEvents')
+def currentEvents():
+    return render_template('currentEvents.html', name="Current Events")
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', name="Blog")
+
 # run the app
 if __name__ == "__main__":
     app.run(debug=True)
